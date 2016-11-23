8573e0a42bfe18e2e110164cec22bdc39b3f52eb
Sven Marquardt <Marquardt@fzi.de>
2016-11-23 12:43:21 +0100

    fix sizes, start implementing connection

--- assets/js/gridstacktest.js
+++ assets/js/gridstacktest.js
@@ -1,32 +1,67 @@
 /**
  * Created by Sven on 02.11.2016.
  */
-//Items behave like if they always belong to the last gridstack
-//They use its cellheight in order to decide their size
-//They dont trigger add/remove events
+//Items dont trigger add/remove events
  $(function () {
-  var options = {
-    // turns animation on
+  $('.grid-stack-1').gridstack({  //Hier führt Aufruf mit options zu Fehler?! Gleiche Zellhöhe wie letzter Grid
     animate: true,
     height: 1,
     width: 1,        //wie viele passen nebeneinander
     float: false,
     disableResize: true,
     cellHeight: 100,
-    removable: false,
+    removable: true,
     removeTimeout: 100,
-    acceptWidgets: false,
-  };
-  $('.grid-stack-1').gridstack(options);
-  $('#bottom.grid-stack').gridstack(_.defaults({height: 1, width: 5, cellHeight: 300, acceptWidgets: '.grid-stack-item', float:false}, options)); //Verwende options aber ändere Parameter für heights
-   $('#bottom2.grid-stack').gridstack(_.defaults({height: 1, width: 5, cellHeight: 100, acceptWidgets: '.grid-stack-item', float:false}, options)); //Verwende options aber ändere Parameter für heights
-
-   $('.grid-stack-1 .grid-stack-item').draggable({
-     revert: 'invalid',
-     handle: '.grid-stack-item-content',
-     scroll: false,
-     appendTo: 'body'
+  });
+  $('.grid-stack-5').gridstack({
+    animate: true,
+    height: 5,
+    width: 5,
+    float: true,
+    disableResize: true,
+    cellHeight: 300,
+    acceptWidgets:'.grid-stack-item',
+    removable: true,
+    removeTimeout: 100,
+  });
+//  $('#bottom2.grid-stack').gridstack(_.defaults({height: 5}, options)); //Verwende options aber ändere Parameter für heights
+   var items = [
+    {x: 0, y: 0, width: 1, height: 1},
+  ];
+  $('.grid-stack-1').each(function () {
+    var grid = $(this).data('gridstack');
+    _.each(items, function (node) {
+      grid.addWidget($('<div><div class="grid-stack-item-content">Drag<div/><div/>'),
+        node.x, node.y, node.width, node.height);
+    }, this);
+  });
+   var itemsFix = [
+     {x: 0, y: 2, width: 1, height: 1},
+    // {x: 4, y: 2, width: 1, height: 1},
+   ];
+   var itemsVar = [
+     {x: 1, y: 2, width: 1, height: 1},
+     {x: 3, y: 0, width: 1, height: 1},
+     {x: 2, y: 1, width: 1, height: 1},
+     {x: 3, y: 1, width: 1, height: 1},
+   ];
+   $('.grid-stack-5').each(function () {
+     var grid = $(this).data('gridstack');
+     _.each(itemsFix, function (node) {
+       grid.addWidget($('<div data-gs-no-move="yes" data-gs-locked="yes" id="final"><div class="grid-stack-item-content"><div/><div/>'),
+         node.x, node.y, node.width, node.height);
+     }, this);
+     _.each(itemsVar, function (node) {
+       grid.addWidget($('<div><div class="grid-stack-item-content"><div/><div/>'),
+         node.x, node.y, node.width, node.height);
+     }, this);
    });
+   //  $('.grid-stack-1 .grid-stack-item').draggable({
+   //   revert: 'invalid',
+   //   handle: '.grid-stack-item-content',
+   //   scroll: false,
+   //   appendTo: 'body'
+   // });
    // $('.grid-stack').on('dragstop', function(event, ui) {
    //   var grid = this;
    //   var element = event.target;
@@ -37,70 +72,51 @@
    //     alert('Not enough free space to place the widget');
    //   }
    // });
-   $('.grid-stack').on('change', function(event, items) {
-     prompt("change");
-   });
+   // $('.grid-stack').on('change', function(event, items) {
+   //   prompt("change");
+   // });
+   // $('.grid-stack').on('removed', function(event, items) {
+   //     prompt("remove");
+   // });
+   // $('.grid-stack').on('added', function(event, items) {
+   //    prompt("added");
+   // });
 });
 
-
-function runButton(){
-  this.grid = $('.grid-stack').data('gridstack');
-
-  this.addNewWidget = function () {
-    var node = {
-      x: randomIntFromInterval(1,10),
-      y: randomIntFromInterval(0,4),
-      width: 1,
-      height: 1
-    };
-    this.grid.addWidget($('<div class="grid-stack-item" data-gs-no-resize="yes"  data-gs-locked="yes">' +
-        '<div class="grid-stack-item-content"/></div>'),
-      node.x, node.y, node.width, node.height);
-    return false;
-  }.bind(this);
-
-  $('#add-new-widget').click(this.addNewWidget);
-  connection.bind("click", function(conn) {
-    this.addNewWidget;
-  });
-}
-
-function randomIntFromInterval(min,max) {
-  return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-
-
-
-
-
-
-
-
-
-
-
+//
+// function runButton(){
+//   this.grid = $('.grid-stack').data('gridstack');
+//
+//   this.addNewWidget = function () {
+//     var node = {
+//       x: randomIntFromInterval(1,10),
+//       y: randomIntFromInterval(0,4),
+//       width: 1,
+//       height: 1
+//     };
+//     this.grid.addWidget($('<div class="grid-stack-item" data-gs-no-resize="yes"  data-gs-locked="yes">' +
+//         '<div class="grid-stack-item-content"/></div>'),
+//       node.x, node.y, node.width, node.height);
+//     return false;
+//   }.bind(this);
+//
+//   $('#add-new-widget').click(this.addNewWidget);
+//   connection.bind("click", function(conn) {
+//     this.addNewWidget;
+//   });
+// }
+//
+// function randomIntFromInterval(min,max) {
+//   return Math.floor(Math.random()*(max-min+1)+min);
+// }
 
 function connect(source,target) {
- //  var endpointOptions = { isSource:true, isTarget:true };
- //  var sourceEndpoint = jsPlumb.addEndpoint($(source),{ anchor:"BottomCenter" }, endpointOptions );
- //  var targetEndpoint = jsPlumb.addEndpoint($(target), { anchor:"BottomCenter" }, endpointOptions );
-  jsPlumb.makeSource(source, {
-    anchor:"Continuous",
-    endpoint:["Rectangle", { width:40, height:20 }],
-    maxConnections:3
-  });
-  jsPlumb.makeTarget(target, {
-    anchor:"Continuous",
-    endpoint:["Rectangle", { width:40, height:20 }],
-    maxConnections:3
-  });
+ var sourceEndpoint = jsPlumb.addEndpoint($(source),{ anchor:"BottomCenter", isSource:true});
+ var targetEndpoint = jsPlumb.addEndpoint($(target), { anchor:"BottomCenter", isTarget:true});
   jsPlumb.connect({
     source: source,
     target: target
   });
- // jsPlumb.draggable($("#connect1 .endpoint1"), {containment:false});
- // jsPlumb.draggable($(".grid-stack-item-content"));
 }
 
 // $( window ).resize(function() {
@@ -121,23 +137,26 @@ function connect2(source,target) {
 }
 
 jsPlumb.bind("ready", function () {
-  $('.grid-stack').on('dragstop', function(event, ui) {
-    connection2 = connect2("connect1", "connect2");
+  $('.grid-stack').on('dragstop', function (event, ui) {
+    var element = $(event.target);
+    var node = element.data('_gridstack_node');
+    console.log(node);
+    connection2 = connect(element.children(),$('#final .grid-stack-item-content'));
     jsPlumb.revalidate(this);
   });
-  $.draggable({
-    drag: function (event, ui) {
-      jsPlumb.revalidate(this);
-      repaintEverything()
-    }
-  });
-  $.dragstop({
-    drag: function (event, ui) {
-      jsPlumb.revalidate(this);
-      repaintEverything()
-    }
-  });
-  connection = connect2("connect1", "connect2");
+  // $.draggable({
+  //   drag: function (event, ui) {
+  //     jsPlumb.revalidate(this);
+  //     repaintEverything()
+  //   }  q
+  // });
+  // $.dragstop({
+  //   drag: function (event, ui) {
+  //     jsPlumb.revalidate(this);
+  //     repaintEverything()
+  //   }
+  // });
+  // connection = connect2("connect1", "connect2");
 /*  var firstInstance = jsPlumb.getInstance({
     PaintStyle: {
       lineWidth: 10,
