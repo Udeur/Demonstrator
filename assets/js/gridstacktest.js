acca886ca843ac2d8f3e3fa8d4f0ea76844a7207
Sven Marquardt <Marquardt@fzi.de>
2016-12-29 11:52:02 +0100

    automatic connections

--- assets/js/gridstacktest.js
+++ assets/js/gridstacktest.js
@@ -2,10 +2,8 @@
  * Created by Sven on 02.11.2016.
  */
 //Items dont trigger add/remove events
-
-jsPlumb.bind("ready", function() {
-//$(document).ready(function() {
-  var w_height=$(window).height();;
+$(document).ready(function() {
+  var w_height=$(window).height();
   var allGrids=$('.grid-stack');
   var topGrids=$('.grid-stack-1');
   var bottomGrids=$('.grid-stack-5');
@@ -17,7 +15,7 @@ jsPlumb.bind("ready", function() {
     removeTimeout: 100,
     minWidth:0,
     verticalMargin: 0 //kein Abstand nach oben
-  }
+  };
 
   window.addEventListener("resize", function (event) {          //Zeichnet die Endpoints neu wenn sich Fenstergröße verändert
     w_height=$(window).height();
@@ -144,110 +142,185 @@ jsPlumb.bind("ready", function() {
       }
     }
   });
+  jsPlumb.bind("ready", function() {
+    jsPlumb.setContainer("bottomGrid");
+    jsPlumb.registerEndpointTypes({
+      "source": {
+        paintStyle: {fill: "white", outlineStroke:"black", outlineWidth:1},
+        hoverPaintStyle: {fill: "green"},
+        connectorStyle: {stroke: "black", strokeWidth: 3}
+      },
+      "target": {
+        paintStyle: {fill: "white", outlineStroke:"black", outlineWidth:1},
+        hoverPaintStyle: {fill: "blue"}
+      }
+    });
 
-  jsPlumb.setContainer("bottomGrid");
-  jsPlumb.registerEndpointTypes({
-    "source": {
-      paintStyle: {fill: "white", outlineStroke:"black", outlineWidth:1},
-      hoverPaintStyle: {fill: "green"},
-      connectorStyle: {stroke: "black", strokeWidth: 3}
-    },
-    "target": {
-      paintStyle: {fill: "white", outlineStroke:"black", outlineWidth:1},
-      hoverPaintStyle: {fill: "blue"}
-    }
-  });
+    jsPlumb.addEndpoint($('.grid-stack-5 #origin.grid-stack-item .grid-stack-item-content'), {
+      anchor: [1, 0.5, 1, 0],
+      maxConnections: -1,
+      type: "source",
+      isSource: true,
+      connector:[ "Flowchart", { stub:10, cornerRadius:5 } ]
+    });
+    jsPlumb.addEndpoint($('.grid-stack-5 #end.grid-stack-item .grid-stack-item-content'), {
+      anchor: [0, 0.5, -1, 0],
+      maxConnections: -1,
+      type: "target",
+      isTarget: true
+    });
+    jsPlumb.addEndpoint($('.grid-stack-5 .grid-stack-item:not(#origin, #end, #block) .grid-stack-item-content'),{
+      anchor: [1, 0.5, 1, 0],
+      maxConnections: -1,
+      type: "source",
+      isSource: true,
+      connector:[ "Flowchart", { stub:10, cornerRadius:5} ]
+    });
+    jsPlumb.addEndpoint($('.grid-stack-5 .grid-stack-item:not(#origin, #end, #block) .grid-stack-item-content'),{
+      anchor: [0, 0.5, -1, 0],
+      maxConnections: -1,
+      type: "target",
+      isTarget: true
+    });
 
-  jsPlumb.addEndpoint($('.grid-stack-5 #origin.grid-stack-item .grid-stack-item-content'), {
-    anchor: [1, 0.5, 1, 0],
-    maxConnections: -1,
-    type: "source",
-    isSource: true,
-    connector:[ "Flowchart", { stub:10, cornerRadius:5 } ]
-  });
-  jsPlumb.addEndpoint($('.grid-stack-5 #end.grid-stack-item .grid-stack-item-content'), {
-    anchor: [0, 0.5, -1, 0],
-    maxConnections: -1,
-    type: "target",
-    isTarget: true
-  });
-  jsPlumb.addEndpoint($('.grid-stack-5 .grid-stack-item:not(#origin, #end, #block) .grid-stack-item-content'),{
-    anchor: [1, 0.5, 1, 0],
-    maxConnections: -1,
-    type: "source",
-    isSource: true,
-    connector:[ "Flowchart", { stub:10, cornerRadius:5} ]
-  });
-  jsPlumb.addEndpoint($('.grid-stack-5 .grid-stack-item:not(#origin, #end, #block) .grid-stack-item-content'),{
-    anchor: [0, 0.5, -1, 0],
-    maxConnections: -1,
-    type: "target",
-    isTarget: true
+    $('.grid-stack-5 #origin.grid-stack-item .grid-stack-item-content').each(function () {                                              //unterer Gridstack wird initialisiert
+      var $items = $('.grid-stack-5 .grid-stack-item:not(#origin, #end, #block)');
+      function checkFirst(item) {
+        var node = $(item).data('_gridstack_node');
+        return node.x==1;
+      }
+      function checkSecond(item) {
+        var node = $(item).data('_gridstack_node');
+        return node.x==2;
+      }
+      function checkThird(item) {
+        var node = $(item).data('_gridstack_node');
+        return node.x==3;
+      }
+      var firstItems=jQuery.grep($items,checkFirst);
+      var secondItems=jQuery.grep($items,checkSecond);
+      var thirdItems=jQuery.grep($items,checkThird);
+
+      function connectEndpoints(sourceEndpoint, targetLevel){
+        if(targetLevel.length>2) {
+          targetLevel.splice((Math.random() * targetLevel.length), 1);
+        }
+        $(targetLevel).each(function () {
+          jsPlumb.connect({
+            source: sourceEndpoint,
+            target: jsPlumb.selectEndpoints({target: this.firstChild}).get(0),
+          });
+          $(this).data('isConnected', true);
+        });
+      }
+
+      function connectEndpointsEnd(sourceEndpoint){
+       jsPlumb.connect({
+          source: sourceEndpoint,
+          target: jsPlumb.selectEndpoints({target: $('.grid-stack-5 #end.grid-stack-item .grid-stack-item-content')}).get(0),
+        });
+        $(this).data('isConnected', true);
+      }
+      function connectOrigin() {
+        var sourceEndpoint= jsPlumb.selectEndpoints({source: $('.grid-stack-5 #origin.grid-stack-item .grid-stack-item-content')}).get(0);
+        if(firstItems.length>0) {
+          connectEndpoints(sourceEndpoint, firstItems);
+        }
+        else{
+          if(secondItems.length>0) {
+            connectEndpoints(sourceEndpoint, secondItems);
+          }
+          else{
+            if(thirdItems.length>0) {
+              connectEndpoints(sourceEndpoint, thirdItems);
+            }
+            else{
+              connectEndpointsEnd(sourceEndpoint);
+            }
+          }
+        }
+      }
+      function connectFirst() {
+        $(firstItems).each(function () {
+          if ($(this).data('isConnected') == true) {
+            var sourceEndpoint = jsPlumb.selectEndpoints({source: this.firstChild}).get(0);
+            if (secondItems.length > 0) {
+              connectEndpoints(sourceEndpoint, secondItems);
+            }
+            else {
+              if (thirdItems.length > 0) {
+                connectEndpoints(sourceEndpoint, thirdItems);
+              }
+              else {
+                connectEndpointsEnd(sourceEndpoint);
+              }
+            }
+          }
+        });
+      }
+      function connectSecond() {
+        $(secondItems).each(function () {
+          if ($(this).data('isConnected') == true) {
+            var sourceEndpoint=jsPlumb.selectEndpoints({source: this.firstChild}).get(0);
+            if (thirdItems.length > 0) {
+              connectEndpoints(sourceEndpoint, thirdItems);
+            }
+            else {
+              connectEndpointsEnd(sourceEndpoint);
+            }
+          }
+        });
+      }
+      function connectThird() {
+        $(thirdItems).each(function () {
+          if ($(this).data('isConnected') == true) {
+            var sourceEndpoint=jsPlumb.selectEndpoints({source: this.firstChild}).get(0);
+            connectEndpointsEnd(sourceEndpoint);
+          }
+        });
+      }
+
+      connectOrigin();
+      connectFirst();
+      connectSecond();
+      connectThird();
+    });
+
+    var isGrid;
+
+    allGrids.on('dragstart', function(event, ui) {
+      isGrid=$(event.target).data('gridstack');
+    });
+
+    allGrids.on('change', function(event, items) {
+      if(isGrid==$(event.target).data('gridstack')){
+        console.log("nothingChanged")
+      }
+      else {
+        console.log("Changed");
+        _.each(items, function (node) {
+          var selectedItemContent=node.el.children(":first");
+          if (jsPlumb.selectEndpoints({element:selectedItemContent}).length == 0) {     //geht in Schlaufe falls Element aus Scrollbar hinzugefügt wurde
+            jsPlumb.addEndpoint((selectedItemContent), {
+              anchor: [1, 0.5, 1, 0],
+              maxConnections: -1,
+              type: "source",
+              isSource: true,
+              connector:[ "Flowchart", { stub:10, cornerRadius:5} ]
+            });
+            jsPlumb.addEndpoint((selectedItemContent), {
+              anchor: [0, 0.5, -1, 0],
+              maxConnections: -1,
+              type: "target",
+              isTarget: true
+            });
+          }
+        });
+      }
+      jsPlumb.repaintEverything();
+    });
   });
   jsPlumb.repaintEverything();
-
-  $('.grid-stack-5 #origin.grid-stack-item .grid-stack-item-content').each(function () {                                              //unterer Gridstack wird initialisiert
-    var $items = $('.grid-stack-5 .grid-stack-item:not(#origin, #end, #block)');
-    function checkFirst(item) {
-      var node = $(item).data('_gridstack_node');
-      return node.x==1;
-    }
-    function checkSecond(item) {
-      var node = $(item).data('_gridstack_node');
-      return node.x==2;
-    }
-    function checkThird(item) {
-      var node = $(item).data('_gridstack_node');
-      return node.x==3;
-    }
-    var firstItems=jQuery.grep($items,checkFirst);
-    var secondItems=jQuery.grep($items,checkSecond);
-    var thirdItems=jQuery.grep($items,checkThird);
-    console.log($('.grid-stack-5 #origin.grid-stack-item .grid-stack-item-content'));
-    $(firstItems).each(function () {
-      console.log(this.firstChild);
-      console.log(jsPlumb.selectEndpoints({target: this.firstChild}));
-      jsPlumb.connect({
-        source: jsPlumb.selectEndpoints({source: $('.grid-stack-5 #origin.grid-stack-item .grid-stack-item-content')}).get(0),
-        target: jsPlumb.selectEndpoints({target: this.firstChild}).get(0)
-      });
-    });
-    console.log(firstItems);
-    console.log(secondItems.length);
-    console.log(thirdItems.length);
-  });
-
-  var isGrid;
-
-  allGrids.on('dragstart', function(event, ui) {
-    isGrid=$(event.target).data('gridstack');
-  });
-
-  allGrids.on('change', function(event, items) {
-    if(isGrid==$(event.target).data('gridstack')){
-      console.log("nothingChanged")
-    }
-    else {
-      console.log("Changed");
-      _.each(items, function (node) {
-        var selectedItemContent=node.el.children(":first");
-        if (jsPlumb.selectEndpoints({element:selectedItemContent}).length == 0) {     //geht in Schlaufe falls Element aus Scrollbar hinzugefügt wurde
-          jsPlumb.addEndpoint((selectedItemContent), {
-            anchor: [1, 0.5, 1, 0],
-            maxConnections: -1,
-            type: "source",
-            isSource: true,
-            connector:[ "Flowchart", { stub:10, cornerRadius:5} ]
-          });
-          jsPlumb.addEndpoint((selectedItemContent), {
-            anchor: [0, 0.5, -1, 0],
-            maxConnections: -1,
-            type: "target",
-            isTarget: true
-          });
-        }
-      });
-    }
-    jsPlumb.repaintEverything();
-  });
 });
+
+
